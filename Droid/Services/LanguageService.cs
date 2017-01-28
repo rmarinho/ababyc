e4881fa9f8e2576272014b8587b0f28a534d8866
Rui Marinho <me@ruimarinho.net>
2017-01-28 02:21:21 +0000

    [Droid] Add Language, Sound and Speak service

---
+++ Droid/Services/LanguageService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ababyc.Models;
+using ababyc.Services;
+using Java.Util;
+
+namespace ababyc.Droid
+{
+	public class LanguageService : ILanguageService
+	{
+		static Language _defaultLanguage;
+		static List<Language> _availableLanguages;
+
+		public Language DefaultLanguage => _defaultLanguage ?? (_defaultLanguage = GetLanguages().FirstOrDefault((arg) => arg.Id == Locale.Default.Language));
+		public IList<Language> GetLanguages() => _availableLanguages ?? (_availableLanguages = Locale.GetAvailableLocales().Select((arg) => new Language { Id = arg.Language, Locale = arg.Language, FriendlyName = arg.DisplayLanguage }).ToList());
+
+	}
+}
